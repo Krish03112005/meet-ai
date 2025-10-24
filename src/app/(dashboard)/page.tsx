fad65d928f509ca8b270f624fd0a680f6de53be0
Krishnandu Gurey <152511535+Krish03112005@users.noreply.github.com>
2025-10-24 16:20:35 +0530

    06: dashoard sidebar

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,11 @@
-import { auth } from "@/lib/auth"
-import { HomeView } from "@/modules/home/ui/views/home-view"
-import { redirect } from "next/navigation";
 import { headers } from "next/headers"
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth"
+
+import { HomeView } from "@/modules/home/ui/views/home-view"
+
+//http://localhost:3000
 
 const Page = async () => {
   const session = await auth.api.getSession({
